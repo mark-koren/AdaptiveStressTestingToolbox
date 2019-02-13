e3d8567a424253a773adce571411eef7b71851d3
Mark Koren <mkoren@stanford.edu>
2019-02-12 16:44:19 -0800

    merged

--- Toolbox/scripts/run_default.sh
+++ Toolbox/scripts/run_default.sh
@@ -5,7 +5,7 @@ cd ../..
 unset PYTHONPATH
 #export PYTHONPATH=$(pwd):$(pwd)/rltools:$(pwd)/garage:$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$PWD/Toolbox/garage
-export PYTHONPATH=$PYTHONPATH:$PWD/Tests
+export PYTHONPATH=$PYTHONPATH:$PWD/TestCases
 export PYTHONPATH=$PYTHONPATH:$PWD/Toolbox
 #pydot from https://github.com/nlhepler/pydot
 #neet run python setup.py install
@@ -28,8 +28,8 @@ for run_name in "$@"; do
     args="$args $line"
     echo "Text read from file: $line"
   done < "$path/$run_name/$run_name.txt"
-  echo "python ../../Tests/$runner $args"
-  python ../../Tests/$runner $args --log_dir $path/$run_name
+  echo "python ../../TestCases/$runner $args"
+  python ../../TestCases/$runner $args --log_dir $path/$run_name
   mkdir -p ~/box/Research/Data/DRL-AST/$group/$run_name
   cp -a $path/$run_name/. ~/box/Research/Data/DRL-AST/$group/$run_name/ &>/dev/null &
 done
